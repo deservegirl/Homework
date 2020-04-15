6f10426e4d58cf63c255f31c29e5fed8c5439f21
laierr <rengirde@yandex.ru>
2020-04-15 20:24:03 +0300

    Number three.1

---
+++ Nrw.java
@@ -0,0 +1,22 @@
+package firstTask;
+
+/**
+ * Created by laierr on 15.04.2020.
+ */
+
+import java.util.Scanner;
+
+public class Nrw  {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in) ;
+        int bit = s.nextInt();
+        int result = minFunction(bit);
+        System.out.println(result );
+
+    }
+
+    public static int minFunction(int n) {
+        return -~((n-~(n^(n+1))/2)^(n-~(n^(n+1))/2+1))/2 ;
+    }
+}
+
