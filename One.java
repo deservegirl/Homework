be480b8c66aee6df4170d7f2c092f1dc36584243
laierr <rengirde@yandex.ru>
2020-07-13 10:40:42 +0300

    Number three.1 2

---
+++ One.java
@@ -0,0 +1,31 @@
+package firstTask;
+
+import java.util.*;
+public class One
+{ public static void main(String[] args ){
+    {
+        Scanner con = new Scanner(System.in);
+
+        int n = con.nextInt();
+
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++)
+
+            arr[i] = con.nextInt();
+
+        Arrays.sort(arr);
+
+        int number = 0;
+
+        for (int i = 0; i < n - 1; i++)
+
+            if (arr[i] != arr[i + 1])
+
+                number++;
+
+        System.out.println(number + 1);
+
+    }
+}
+}
